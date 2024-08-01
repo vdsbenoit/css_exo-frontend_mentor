b36cc6dab5d7a315f140637d4081faac3205167c
vdsbenoit <vdsbenoit@gmail.com>
2024-08-01 17:39:35 +0200

    exo-1_social_links_card

--- app.vue
+++ app.vue
@@ -1,3 +1,38 @@
 <template>
-  
+  <div class="flex min-h-screen items-center justify-center">
+    <div class="bg-grey-800 w-96 rounded-xl px-8 py-9">
+      <img
+        class="mx-auto block h-24 rounded-full"
+        src="/assets/images/avatar-jessica.jpeg"
+      />
+      <div class="text-center">
+        <h4 class="mt-6 text-2xl tracking-wider">Jessica Randall</h4>
+        <h5 class="text-green mt-2 text-sm font-semibold">
+          London, United Kingdom
+        </h5>
+        <p class="mt-6 text-sm text-neutral-300">
+          "Front-end developer and avid reader."
+        </p>
+        <div class="mt-5 space-y-4">
+          <a
+            v-for="link in links"
+            :key="link.name"
+            class="bg-grey-700 block rounded-md p-3 text-sm font-semibold"
+            :href="link.target"
+            target="_blank"
+            >{{ link.name }}</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+<script lang="ts" setup>
+const links = [
+  { name: "GitHub", target: "https://GitHub.com" },
+  { name: "Frontend Mentor", target: "https://Frontend-Mentor.com" },
+  { name: "LinkedIn", target: "https://LinkedIn.com" },
+  { name: "Twitter", target: "https://Twitter.com" },
+  { name: "Instagram", target: "https://Instagram.com" },
+];
+</script>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
-export default <Partial<Config>> {
-  content: [
-    'docs/content/**/*.md'
-  ]
-}
+export default <Partial<Config>>{
+  content: ["docs/content/**/*.md"],
+  theme: {
+    extend: {
+      colors: {
+        green: "hsl(75, 94%, 57%)",
+        white: "hsl(0, 0%, 100%)",
+        grey: {
+          700: "hsl(0, 0%, 20%)",
+          800: "hsl(0, 0%, 12%)",
+          900: "hsl(0, 0%, 8%)",
+        },
+      },
+    },
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+    },
+  },
+};
