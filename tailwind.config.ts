57c7dd798de06106d04445bdc59a56ebe4958b6f
vdsbenoit <vdsbenoit@gmail.com>
2024-08-01 17:47:36 +0200

    Add font & body background config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,14 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
-export default <Partial<Config>> {
-  content: [
-    'docs/content/**/*.md'
-  ]
-}
+export default <Partial<Config>>{
+  content: ["docs/content/**/*.md"],
+  theme: {
+    extend: {
+      colors: {},
+    },
+    fontFamily: {
+      sans: ["Arial", ...defaultTheme.fontFamily.sans],
+    },
+  },
+};
