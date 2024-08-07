5a746217602d235b1fd9f463f6e3212e7a8d256e
vdsbenoit <vdsbenoit@gmail.com>
2024-08-07 18:33:44 +0200

    Responsive design

--- components/exo.vue
+++ components/exo.vue
@@ -3,24 +3,28 @@
     <main
       class="grid max-w-[375px] bg-gray-50 md:max-w-4xl md:grid-cols-2 md:rounded-xl"
     >
-      <section id="calc" class="p-8">
-        <div class="flex items-center justify-between">
-          <h2 class="font-bold text-slate-800">Mortage Calculator</h2>
-          <span class="text-xs font-semibold text-slate-400 underline">
+      <section id="calc" class="p-5 md:p-8">
+        <div class="items-center justify-between md:flex">
+          <h2 class="mb-1 text-slate-800">Mortage Calculator</h2>
+          <span
+            class="text-sm font-medium text-slate-400 underline md:font-semibold"
+          >
             Clear All
           </span>
         </div>
         <form class="mt-4 grid grid-cols-1 gap-4 md:grid-cols-2">
           <label class="md:col-span-2">
-            <span class="text-sm font-semibold text-slate-500">
+            <span class="text-sm text-slate-500 md:font-semibold">
               Mortgage Amount
             </span>
             <div
               class="mt-2 flex w-full overflow-hidden rounded-md border border-slate-400"
             >
-              <span class="bg-slate-50 px-3 py-2 font-bold text-slate-700"
-                >£</span
+              <span
+                class="content-center bg-slate-50 px-3 font-bold text-slate-700"
               >
+                £
+              </span>
               <input
                 type="string"
                 value="300,000"
@@ -30,7 +34,7 @@
             </div>
           </label>
           <label>
-            <span class="text-sm font-semibold text-slate-500">
+            <span class="text-sm text-slate-500 md:font-semibold">
               Mortgage Term
             </span>
             <div
@@ -40,15 +44,17 @@
                 type="number"
                 :value="25"
                 name="term"
-                class="w-full px-3 py-2 font-bold text-slate-800 focus:outline-none"
+                class="w-full border-none px-3 py-2 font-bold text-slate-800 focus:outline-none"
               />
-              <span class="bg-slate-50 px-3 py-2 font-bold text-slate-700"
-                >years</span
+              <span
+                class="content-center bg-slate-50 px-3 font-bold text-slate-700"
               >
+                years
+              </span>
             </div>
           </label>
           <label>
-            <span class="text-sm font-semibold text-slate-500">
+            <span class="text-sm text-slate-500 md:font-semibold">
               Interest Rate
             </span>
             <div
@@ -58,19 +64,21 @@
                 type="number"
                 :value="2.25"
                 name="rate"
-                class="w-full px-3 py-2 font-bold text-slate-800 focus:outline-none"
+                class="w-full border-none px-3 py-2 font-bold text-slate-800 focus:outline-none"
               />
-              <span class="bg-slate-50 px-3 py-2 font-bold text-slate-700"
-                >%</span
+              <span
+                class="content-center bg-slate-50 px-3 font-bold text-slate-700"
               >
+                %
+              </span>
             </div>
           </label>
-          <fieldset class="col-span-2">
-            <legend class="text-sm font-semibold text-slate-500">
+          <fieldset class="md:col-span-2">
+            <legend class="text-sm text-slate-500 md:font-semibold">
               Mortgage type
             </legend>
             <label
-              class="mt-2 block w-full rounded py-2 text-sm font-bold text-slate-700 outline outline-1 outline-gray-500 has-[:checked]:bg-yellow-50 has-[:checked]:outline-lime"
+              class="mt-2 flex w-full items-center rounded py-2 font-bold text-slate-700 outline outline-1 outline-gray-500 has-[:checked]:bg-yellow-50 has-[:checked]:outline-lime"
             >
               <input
                 class="form-radio me-3 ms-4 text-lime focus:outline-lime focus:ring-0"
@@ -83,7 +91,7 @@
               Repayment
             </label>
             <label
-              class="mt-2 block w-full rounded py-2 text-sm font-bold text-slate-700 outline outline-1 outline-gray-500 has-[:checked]:bg-yellow-50 has-[:checked]:outline-lime"
+              class="mt-2 flex w-full items-center rounded py-2 font-bold text-slate-700 outline outline-1 outline-gray-500 has-[:checked]:bg-yellow-50 has-[:checked]:outline-lime"
             >
               <input
                 class="form-radio me-3 ms-4 text-lime focus:outline-lime focus:ring-0"
@@ -95,10 +103,10 @@
               Interest Only
             </label>
           </fieldset>
-          <div class="col-span-2 flex">
+          <div class="flex justify-center md:col-span-2 md:justify-start">
             <button
               type="submit"
-              class="mt-4 inline-flex items-center justify-center rounded-full bg-lime px-8 py-3 font-bold text-slate-800"
+              class="mt-4 inline-flex w-full items-center justify-center rounded-full bg-lime px-2 py-3 font-bold text-slate-800 md:w-auto md:px-8"
             >
               <UIcon class="me-3 size-5" name="i-heroicons-calculator-solid" />
               Calculate Repayments
@@ -108,27 +116,29 @@
       </section>
       <section
         id="results"
-        class="rounded-r-xl rounded-bl-[40px] bg-slate-800 p-9"
+        class="bg-slate-900 p-5 md:rounded-r-xl md:rounded-bl-[40px] md:p-8"
       >
-        <h2 class="text-gray-200">Your results</h2>
-        <p class="mt-2 text-sm text-gray-200">
+        <h2 class="mt-2 text-gray-200 md:mt-0">Your results</h2>
+        <p class="mt-4 text-sm text-slate-300 md:mt-2">
           Your results are shown below based on the information you provided. To
           adjust the results, edit the form and click “calculate repayments”
           again.
         </p>
         <div
-          class="mt-8 grid grid-cols-1 divide-y divide-gray-700 rounded-lg border-t-4 border-lime bg-slate-900 px-6"
+          class="mt-5 grid grid-cols-1 divide-y divide-gray-700 rounded-lg border-t-4 border-lime bg-slate-950 px-4 md:mt-8 md:px-6"
         >
-          <div class="py-6">
+          <div class="py-4 md:py-6">
             <h3>You monthly repayments</h3>
-            <div class="text-5xl font-bold lining-nums tabular-nums text-lime">
+            <div
+              class="text-3xl font-bold lining-nums tabular-nums text-lime md:text-5xl"
+            >
               {{ getMonthlyRepayments() }}
             </div>
           </div>
-          <div class="py-5">
+          <div class="py-4 md:py-5">
             <h3>Total you'll repay over the term</h3>
             <div
-              class="text-xl font-bold lining-nums tabular-nums text-gray-300"
+              class="text-xl font-bold lining-nums tabular-nums text-gray-200"
             >
               {{ getTotalRepay() }}
             </div>
@@ -160,9 +170,9 @@ const getTotalRepay = () => {
 
 <style scoped lang="postcss">
 h2 {
-  @apply text-lg font-bold tracking-wide;
+  @apply text-xl font-bold tracking-wide md:text-lg;
 }
 h3 {
-  @apply mb-2 text-sm text-gray-500;
+  @apply mb-2 text-sm text-gray-400;
 }
 </style>
